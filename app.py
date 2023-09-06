279c72d21101009683c25cd0fa93adc0f99e0a98
dudeshi69 <77315458+dudeshi69@users.noreply.github.com>
2023-09-07 01:23:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,25 +7,25 @@ db_params = {
     "database": "PropReturns",
     "user": "postgres",
     "password": "Dhyey@16",
-    "port": 5436,
+    "port": 5435,
 }
 
 @app.route('/get_data_by_document_no', methods=['GET'])
 def get_data_by_document_no():
-    document_no = request.args.get('दस्त क्र.')
+    document_no = request.args.get('document_number')
     conn = psycopg2.connect(**db_params)
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM "MBProp" WHERE "दस्त क्र." = %s', (document_no,))
+    cursor.execute('SELECT * FROM "mbprop" WHERE "document_number" = %s', (document_no,))
     data = cursor.fetchall()
     conn.close()
     return jsonify(data)
 
 @app.route('/get_data_by_year_of_registration', methods=['GET'])
 def get_data_by_year_of_registration():
-    year_of_registration = request.args.get('वर्ष')
+    year_of_registration = request.args.get('year_year')
     conn = psycopg2.connect(**db_params)
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM "MBProp" WHERE "वर्ष" = %s', (year_of_registration,))
+    cursor.execute('SELECT * FROM "mbprop" WHERE "year_year" = %s', (year_of_registration,))
     data = cursor.fetchall()
     conn.close()
     return jsonify(data)
